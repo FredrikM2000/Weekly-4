c4a1bc4499f50cd89d34da024837b5db33cb5315
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2020-09-29 16:11:01 +0200

    Add project files.

---
+++ Weekly 4.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+
+char converter(char i) {
+	return toupper(i);
+}
+
+void task1() {
+	std::string message;
+	std::cout << "Enter a message : ";
+	std::getline(std::cin, message);
+
+	for (int i = 0; i < message.size(); i++) {
+		message.at(i) = converter(message.at(i));
+	}
+	std::cout << message;
+}
+
+void task2() {
+
+	char arr[100]{};
+	arr[20] = '/';
+	arr[89] = ' ';
+	int n = 0;
+	
+	bool win = false;
+
+	do {
+		arr[n] = 'O';
+		for (int u = 0; u < sizeof(arr); u++) {
+			std::cout << "|" << arr[u];
+			if (u % 10 == 0 && u > 1) {
+				std::cout << "\n";
+			}
+		}
+
+		std::cout << "\nEnter a letter : ";
+		char m = _getch();
+
+		arr[n] = ' ';
+		switch (m) {
+		case('w'):
+			n -= 10;
+			break;
+		case('s'):
+			n += 10;
+			break;
+		case('a'):
+			n -= 1;
+			break;
+		case('d'):
+			n += 1;
+			break;
+		default:
+			std::cout << "Not valid";
+		}
+		if (arr[n] == '/') {
+			n += 10;
+		}
+
+
+		system("cls");
+	} while (win == false);
+	system("pause");
+}
+void task3() {
+
+}
+void DiceTask() {
+
+}
+
+
+
+int main() {
+	//task1();
+	task2();
+}
